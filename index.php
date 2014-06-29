60ebc561b21bb7e4645b00a4727e2b647fa229e0
William Mendoza <williamjmendoza@gmail.com>
2014-06-29 12:15:52 -0430

    Ejemplo de consulta básica de sparql

--- index.php
+++ index.php
@@ -3,4 +3,32 @@
 	include(dirname(__FILE__)."/init.php");
 	
 	echo "Bienvenido a sigecost";
+	
+	$query = '
+  		SELECT DISTINCT ?subject ?property ?object WHERE { 
+			?subject ?property ?object . 
+		}
+	';
+	
+	$r = '';
+	
+	$rows = $GLOBALS['ONTOLOGIA_STORE']->query($query, 'rows');
+	
+	if ($errors = $GLOBALS['ONTOLOGIA_STORE']->getErrors()) {
+		error_log("arc2sparql error:\n" . join("\n", $errors));
+		echo "arc2sparql error:\n" . join("\n", $errors);
+	} else {
+		if($rows){
+			$r = '<table border=1> <th>N</th><th>Subject</th><th>Property</th><th>Object</th>'."\n";
+			$count = 0;
+			foreach ($rows as $row) {
+				$r .= '<tr><td>'.(++$count).'</td><td>'.$row['subject'] .  '</td><td>'.$row['property'] . '</td><td>'.$row['object'] . '</td></tr>'."\n";
+			}
+			$r .='</table>'."\n";
+		} else {
+			$r = '<em>No data returned</em>';
+		}
+	}
+	
+	echo $r;
 ?>
