92e75dce60c288543a83e719fb5589b48da5b607
William Mendoza <williamjmendoza@gmail.com>
2014-07-24 00:52:27 -0430

    Prueba de bootstrap

---
+++ vista/prueba.php
@@ -0,0 +1,140 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+	
+		<meta charset="utf-8">
+		<!-- Para estar seguro de que se utiliza el modo de generación de gráficos más reciente de IE. -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- En dispositivos móviles, para garantizar la generación de gráficos correcta y un funcionamiento correcto de zoom de toque de pantalla. -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Prueba de bootstrap</title>
+		
+		<!-- Bootstrap -->
+    	<link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    	
+    	<style type="text/css">
+    	/*
+    		div > div > div {
+    			border-width: 1px; border-color: red; border-style: solid;
+    		}
+    		*/
+    		
+    		body { padding-top: 70px; }
+    	</style>
+    	
+    	<?php 
+    	/*
+    	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+		*/				
+		?>
+	
+	</head>
+	
+	<body>
+	
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container">
+				
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Sigecost</a>
+				</div>
+				
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Enlace 1</a></li>
+						<li><a href="#">Enlace 2</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Enlace 3 <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Enlace 3.1</a></li>
+								<li><a href="#">Enlace 3.2</a></li>
+								<li><a href="#">Enlace 3.3</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Enlace 3.2.1</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Enlace 3.3.2</a></li>
+							</ul>
+						</li>
+					</ul>
+      
+				</div><!-- /.navbar-collapse -->
+		  	
+			</div>
+		</nav>
+		
+		<div class="container">
+			<div class="alert alert-danger alert-dismissible" role="alert">
+				<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<strong>¡Oh, cielos!</strong> <a href="#" class="alert-link">Cambie unas pocas cosas</a> e inténtelo nuevamente.
+			</div>
+		</div>
+		
+		<div class="container">
+			<div class="jumbotron">
+			
+				<h1>Hello, world!</h1>
+				<p>
+					Esta es una unidad de héroe sencilla, un componente simple de estilo de jumbotron
+					para llamar la atención al contenido especial o información.
+				</p>
+				<p><a class="btn btn-primary btn-lg" role="button">Leer m&aacute;s</a></p>
+			</div>
+		</div>
+		
+		<div class="container">
+			<h1>Hola</h1>
+			<h1>William</h1>
+			<h1>Sigecost</h1>
+			<h1>Infocentro</h1>
+			<h1>UCV</h1>
+			<h1>Computaci&oacute;n</h1>
+		</div>
+
+		
+		
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		
+	
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	    <script src="../lib/jquery/jquery-1.11.1.min.js"></script>
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+	
+	</body>
+
+</html>
