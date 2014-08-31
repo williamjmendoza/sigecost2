03269b1b7bb9c8692457aefeb9ab0f36d5642d66
MigdaliaMedina <migdaeme@gmail.com>
2014-08-31 12:55:48 -0430

    agregada paginacion en ET "consumible"

--- controlador/instancia/elementoTecnologico/consumible.php
+++ controlador/instancia/elementoTecnologico/consumible.php
@@ -1,24 +1,54 @@
 <?php
 
 	require_once( dirname(__FILE__) . '/../../../init.php' );
-	
+
 	// Controladores
 	require_once ( SIGECOST_PATH_CONTROLADOR . '/controlador.php' );
-	
+	require_once ( SIGECOST_PATH_CONTROLADOR . '/paginacion.php' );
+
 	// Modelos
 	require_once ( SIGECOST_PATH_MODELO . '/instancia/elementoTecnologico/consumible.php' );
-	
+
 	class ControladorInstanciaETConsumible extends Controlador
 	{
+		use ControladorTraitPaginacion;
+
 		public function buscar()
 		{
-			$consumibles = ModeloInstanciaETConsumible::buscarConsumibles();
-					
+			// Obtener el formulario
+			$form = FormularioManejador::getFormulario(FORM_INSTANCIA_ET_CONSUMIBLE_BUSCAR);
+
+			// Obtener la cantidad total de elementos de instancias que se obtendrán en la bśuqueda
+			$totalElementos = ModeloInstanciaETConsumible::buscarInstanciasTotalElementos();
+
+			// Verificar que no hubo errores consultando el número total de elementos para esta búsqueda
+			if($totalElementos !== false)
+			{
+				// Configurar el objeto de paginación
+				$form->setPaginacion(new EntidadPaginacion($totalElementos));  // EntidadPaginacion(<Tamaño página>, <Total elementos>)
+				$this->__validarParametrosPaginacion($form);
+				$form->getPaginacion()->setUrlObjetivo("consumible.php?accion=buscar");
+			}
+
+			// Realizar la consulta de la búsqueda estableciendo los parámetros para la navegación
+			$parametros = array();
+			// Establecer los parámetros de la navegación para la consulta de la búsqueda
+			if($totalElementos !== false)
+			{
+				$parametros = array(
+						'desplazamiento' => $form->getPaginacion()->getDesplazamiento(),
+						'limite' => $form->getPaginacion()->getTamanoPagina()
+				);
+			}
+
+			$consumibles = ModeloInstanciaETConsumible::buscarConsumibles($parametros);
+
 			$GLOBALS['SigecostRequestVars']['consumibles'] = $consumibles;
-			
+			$GLOBALS['SigecostRequestVars']['formPaginacion'] = $form;
+
 			require ( SIGECOST_PATH_VISTA . '/instancia/elementoTecnologico/consumibleBuscar.php' );
 		}
-		
+
 		public function desplegarDetalles()
 		{
 			if(!isset($_REQUEST['iri']) || ($iri=trim($_REQUEST['iri'])) == ''){
@@ -27,15 +57,15 @@
 				$this->__desplegarDetalles($iri);
 			}
 		}
-		
+
 		public function guardar()
 		{
 			$form = FormularioManejador::getFormulario(FORM_INSTANCIA_ET_CONSUMIBLE_INSERTAR_MODIFICAR);
-			
+
 			// Validar, obtener y guardar todos los inputs desde el formulario
 			$this->__validarEspecificacion($form);
 			$this->__validarTipo($form);
-			
+
 			// Verificar que no hubo nigún error con los datos suministrados en el formulario
 			if(count($GLOBALS['SigecostErrors']['general']) == 0)
 			{
@@ -44,23 +74,23 @@
 					// Consultar si existe una instancia de consumible con las mismas características
 					if( ($existeConsumible = ModeloInstanciaETConsumible::existeConsumible($form->getConsumible())) === null )
 						throw new Exception("La instancia de consumible no pudo ser guardada.");
-					
+
 					// Validar si existe una instancia de consumible con las mismas características
 					if ($existeConsumible === true)
 						throw new Exception("Ya existe una instancia de consumible con las mismas caracter&iacute;sticas.");
-					
+
 					// Guardar la instancia de consumible en la base de datos
 					$iriNuevaInstancia = ModeloInstanciaETConsumible::guardarConsumible($form->getConsumible());
-					
+
 					// Verificar si ocurrio algún error mientras se guardaba la instancia de consumible
 					if ($iriNuevaInstancia === false)
 						throw new Exception("La instancia de consumible no pudo ser guardada.");
-					
+
 					// Mostrar un mensaje indicando que se ha guardado satisfactoriamente, y mostrar los detalles
 					// de la instancia de consumible guardada
 					$GLOBALS['SigecostInfo']['general'][] = "Instancia de consumible guardada satisfactoriamente.";
 					$this->__desplegarDetalles($iriNuevaInstancia);
-					
+
 				} catch (Exception $e){
 					$GLOBALS['SigecostErrors']['general'][] = $e->getMessage();
 					$this->__desplegarFormulario();
@@ -68,28 +98,32 @@
 			} else {
 				$this->__desplegarFormulario();
 			}
-		}  
-		
+		}
+
 		public function insertar()
 		{
 			$this->__desplegarFormulario();
 		}
-		
+
 		private function __desplegarDetalles($iriInstancia)
 		{
-				
+
 			$consumible = ModeloInstanciaETConsumible::obtenerConsumiblePorIri($iriInstancia);
-				
+
 			$GLOBALS['SigecostRequestVars']['consumible'] = $consumible;
-				
+
 			require ( SIGECOST_PATH_VISTA . '/instancia/elementoTecnologico/consumibleDetalles.php' );
 		}
-		
+
 		private function __desplegarFormulario()
 		{
+
+			$consumibles = ModeloInstanciaETConsumible::obtenerTodosConsumibles();
+			$GLOBALS['SigecostRequestVars']['consumibles'] = $consumibles;
+
 			require ( SIGECOST_PATH_VISTA . '/instancia/elementoTecnologico/consumibleInsertarModificar.php' );
 		}
-		
+
 		private function __validarEspecificacion(FormularioInstanciaETConsumible $form)
 		{
 			if(!isset($_POST['especificacion']) || ($especificacion=trim($_POST['especificacion'])) == ''){
@@ -98,7 +132,7 @@
 				$form->getConsumible()->setEspecificacion($especificacion);
 			}
 		}
-		
+
 		private function __validarTipo(FormularioInstanciaETConsumible $form)
 		{
 			if(!isset($_POST['tipo']) || ($tipo=trim($_POST['tipo'])) == ''){
@@ -108,6 +142,6 @@
 			}
 		}
 	}
-	
+
 	new ControladorInstanciaETConsumible();
 ?>
--- modelo/instancia/elementoTecnologico/consumible.php
+++ modelo/instancia/elementoTecnologico/consumible.php
@@ -2,57 +2,103 @@
 
 	// Entidades
 	require_once( SIGECOST_PATH_ENTIDAD . '/instancia/elementoTecnologico/consumible.php' );
-	
+
 	// Lib
 	require_once( SIGECOST_PATH_LIB . '/definiciones.php' );
-	
+
 	// Modelos
 	require_once ( SIGECOST_PATH_MODELO . '/general.php' );
-	
+
 	class ModeloInstanciaETConsumible
 	{
-		public static function buscarConsumibles()
+		public static function buscarConsumibles(array $parametros = null)
 		{
 			$preMsg = 'Error al buscar las instancias de consumibles.';
 			$consumibles = array();
+			$limite = '';
+			$desplazamiento = '';
+
 			try
 			{
+				if($parametros !== null && count($parametros) > 0){
+					if(isset($parametros['desplazamiento'])) $desplazamiento = 'OFFSET ' . $parametros['desplazamiento'];
+					if(isset($parametros['limite'])) $limite = 'LIMIT ' . $parametros['limite'];
+				}
 				// Obtener las instancias de consumibles
 				$query = '
 						PREFIX : <'.SIGECOST_IRI_ONTOLOGIA_NUMERAL.'>
 						PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-		
+
 						SELECT
 							?iri ?especificacion ?tipo
 						WHERE
 						{
-							?iri rdf:type :'.SIGECOST_FRAGMENTO_CONSUMIBLE.' .
-							?iri :especificacionConsumible ?especificacion .
-							?iri :tipoConsumible ?tipo .
+							'.self::buscarInstanciasSubQuery().'
 						}
 						ORDER BY
 							?especificacion ?tipo
+						'.$desplazamiento.'
+						'.$limite.'
 				';
-		
+
 				$rows = $GLOBALS['ONTOLOGIA_STORE']->query($query, 'rows');
-		
+
 				if ($errors = $GLOBALS['ONTOLOGIA_STORE']->getErrors())
 					throw new Exception($preMsg . "  Detalles:\n". join("\n", $errors));
-		
+
 				if (is_array($rows) && count($rows) > 0){
 					foreach ($rows AS $row){
 						$consumibles[$row['iri']] = self::llenarConsumible($row);
 					}
 				}
-				
+
 				return $consumibles;
-		
+
 			} catch (Exception $e) {
 				error_log($e, 0);
 				return false;
 			}
 		}
-		
+
+		public static function buscarInstanciasTotalElementos(array $parametros = null)
+		{
+			$preMsg = 'Error al buscar el contador de las instancias elemento tecnologico de aplicación gráfica digital, dibujo y diseño..';
+
+			// Buscar la cantidad de instancias de elemento tecnologico aplicación gráfica digital, dibujo y diseño.
+			$query = '
+					PREFIX : <'.SIGECOST_IRI_ONTOLOGIA_NUMERAL.'>
+					PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+					SELECT
+						(COUNT(?iri) AS ?totalElementos)
+					WHERE
+					{
+						'.self::buscarInstanciasSubQuery().'
+					}
+			';
+
+			$rows = $GLOBALS['ONTOLOGIA_STORE']->query($query, 'rows');
+
+			if ($errors = $GLOBALS['ONTOLOGIA_STORE']->getErrors())
+				throw new Exception($preMsg . " Detalles:\n". join("\n", $errors));
+
+			if (is_array($rows) && count($rows) > 0){
+				reset($rows);
+				return current($rows)['totalElementos'];
+			}
+			else return false;
+		}
+
+		public static function buscarInstanciasSubQuery(array $parametros = null)
+		{
+			return
+			'
+				?iri rdf:type :'.SIGECOST_FRAGMENTO_CONSUMIBLE.' .
+				?iri :especificacionConsumible ?especificacion .
+				?iri :tipoConsumible ?tipo .
+			';
+		}
+
 		public static function existeConsumible(EntidadInstanciaETConsumible $consumible)
 		{
 			$preMsg = 'Error al verificar la existencia de una instancia de consumible.';
@@ -60,29 +106,29 @@
 			{
 				if ($consumible === null)
 					throw new Exception($preMsg . ' El parámetro \'$consumible\' es nulo.');
-		
+
 				if ($consumible->getEspecificacion() === null)
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getEspecificacion()\' es nulo.');
-		
+
 				if ($consumible->getEspecificacion() == "")
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getEspecificacion()\' está vacío.');
-		
+
 				if ($consumible->getTipo() === null)
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getTipo()\' es nulo.');
-		
+
 				if ($consumible->getTipo() == "")
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getTipo()\' está vacío.');
-				
+
 				// Si $consumible->getIri() está presente, dicho iri de instancia será ignorado en la verificación
 				$filtro = ($consumible->getIri() !== null && $consumible->getIri() != '')
 					? 'FILTER (?instanciaConsumible != <'.$consumible->getIri().'>) .' : '';
-		
+
 				// Verificar si existe un consumible con la misma especificacion y tipo, que el pasado por parámetros
 				$query = '
 						PREFIX : <'.SIGECOST_IRI_ONTOLOGIA_NUMERAL.'>
 						PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 						PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-		
+
 						ASK
 						{
 							?instanciaConsumible rdf:type :'.SIGECOST_FRAGMENTO_CONSUMIBLE.' .
@@ -91,63 +137,63 @@
 							'.$filtro.'
 						}
 				';
-		
+
 				$result = $GLOBALS['ONTOLOGIA_STORE']->query($query);
-		
+
 				if ($errors = $GLOBALS['ONTOLOGIA_STORE']->getErrors())
 					throw new Exception("Error al consultar la existencia de la instancia de consumible " .
 							"(especificacion = '".$consumible->getEspecificacion()."', tipo = '".$consumible->getTipo().
 							"'). Detalles:\n" . join("\n", $errors));
-		
+
 					return $result['result'];
-		
+
 			} catch (Exception $e) {
 				error_log($e, 0);
 				return null;
 			}
 		}
-		
+
 		// Guarda una nueva instancia de consumible, y retorno su iri
 		public static function guardarConsumible(EntidadInstanciaETConsumible $consumible)
 		{
 			$preMsg = 'Error al guardar el consumible.';
-				
+
 			try
 			{
 				if ($consumible === null)
 					throw new Exception($preMsg . ' El parámetro \'$consumible\' es nulo.');
-		
+
 				if ($consumible->getEspecificacion() === null)
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getEspecificacion()\' es nulo.');
-		
+
 				if ($consumible->getEspecificacion() == "")
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getEspecificacion()\' está vacío.');
-		
+
 				if ($consumible->getTipo() === null)
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getTipo()\' es nulo.');
-		
+
 				if ($consumible->getTipo() == "")
 					throw new Exception($preMsg . ' El parámetro \'$consumible->getTipo()\' está vacío.');
-		
+
 				// Consultar el número de secuencia para la siguiente instancia de consumible a crear.
 				$secuencia = ModeloGeneral::getSiguienteSecuenciaInstancia(SIGECOST_FRAGMENTO_CONSUMIBLE);
-		
+
 				// Validar si hubo errores obteniendo el siguiente número de secuencia de instancia
 				if($secuencia === false)
 					throw new Exception('Error al guardar la instancia de consumible. No se pudo obtener el número '.
 							'de la siguiente secuencia para la instancia de la clase \''.SIGECOST_FRAGMENTO_CONSUMIBLE.'\'');
-		
+
 					// Construir el fragmento de la nueva instancia de consumible
 					// conctenando el framento de la clase sistema operativo "SIGECOST_FRAGMENTO_CONSUMIBLE"
 					// con el el caracater underscore "_" y el número de secuencia obtenido "$secuencia"
 					$fragmentoIriInstancia = SIGECOST_FRAGMENTO_CONSUMIBLE . '_' . $secuencia;
-		
+
 					// Guardar la nueva instancia de consumible
 					$query = '
 						PREFIX : <'.SIGECOST_IRI_ONTOLOGIA_NUMERAL.'>
 						PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 						PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-		
+
 						INSERT INTO <'.SIGECOST_IRI_GRAFO_POR_DEFECTO.'>
 						{
 							:'.$fragmentoIriInstancia.' rdf:type :'.SIGECOST_FRAGMENTO_CONSUMIBLE.' .
@@ -155,53 +201,60 @@
 							:'.$fragmentoIriInstancia.' :tipoConsumible "'.$consumible->getTipo().'"^^xsd:string .
 						}
 				';
-		
+
 					$result = $GLOBALS['ONTOLOGIA_STORE']->query($query);
-		
+
 					if ($errors = $GLOBALS['ONTOLOGIA_STORE']->getErrors())
 						throw new Exception("Error al guardar la instancia de consumible. Detalles:\n". join("\n", $errors));
-									
+
 					return SIGECOST_IRI_ONTOLOGIA_NUMERAL.$fragmentoIriInstancia;
-		
+
 			} catch (Exception $e) {
 				error_log($e, 0);
 				return false;
 			}
 		}
-		
+
 		public static function llenarConsumible($row)
 		{
 			try {
 				$consumible = null;
-		
+
 				if(!is_array($row))
 					throw new Exception('Error al intentar llenar la instancia de consumible. '.
 						'Detalles: el parámetro \'$row\' no es un arreglo.');
-		
+
 				$consumible = new EntidadInstanciaETConsumible();
 				$consumible->setIri($row['iri']);
 				$consumible->setEspecificacion($row['especificacion']);
 				$consumible->setTipo($row['tipo']);
-		
+
 				return $consumible;
-		
+
 			} catch (Exception $e) {
 				error_log($e, 0);
 				return false;
 			}
 		}
-		
+
 		public static function obtenerTodosConsumibles()
 		{
 			$preMsg = 'Error al obtener todas las instancias de consumibles.';
 			$consumibles = array();
+			$limite = '';
+			$desplazamiento = '';
+
 			try
 			{
+				if($parametros !== null && count($parametros) > 0){
+					if(isset($parametros['desplazamiento'])) $desplazamiento = 'OFFSET ' . $parametros['desplazamiento'];
+					if(isset($parametros['limite'])) $limite = 'LIMIT ' . $parametros['limite'];
+				}
 				// Obtener todas las instancias de consumibles
 				$query = '
 						PREFIX : <'.SIGECOST_IRI_ONTOLOGIA_NUMERAL.'>
 						PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-			
+
 						SELECT
 							?iri ?especificacion ?tipo
 						WHERE
@@ -212,43 +265,45 @@
 						}
 						ORDER BY
 							?especificacion ?tipo
+						'.$desplazamiento.'
+						'.$limite.'
 				';
-			
+
 				$rows = $GLOBALS['ONTOLOGIA_STORE']->query($query, 'rows');
-			
+
 				if ($errors = $GLOBALS['ONTOLOGIA_STORE']->getErrors())
 					throw new Exception($preMsg . "  Detalles:\n". join("\n", $errors));
-			
+
 				if (is_array($rows) && count($rows) > 0){
 					foreach ($rows AS $row){
 						$consumibles[$row['iri']] = self::llenarConsumible($row);
 					}
 				}
-			
+
 				return $consumibles;
-			
+
 			} catch (Exception $e) {
 				error_log($e, 0);
 				return false;
 			}
 		}
-		
+
 		public static function obtenerConsumiblePorIri($iri)
 		{
 			$preMsg = 'Error al obtener una instancia de consumible dado el iri.';
-				
+
 			try {
 				if ($iri === null)
 					throw new Exception($preMsg . ' El parámetro \'$iri\' es nulo.');
-		
+
 				if (($iri=trim($iri)) == "")
 					throw new Exception($preMsg . ' El parámetro \'$iri\' está vacío.');
-		
+
 				// Obtener la instancia de consumible dado el iri
 				$query = '
 						PREFIX : <'.SIGECOST_IRI_ONTOLOGIA_NUMERAL.'>
 						PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-		
+
 						SELECT
 							?iri ?especificacion ?tipo
 						WHERE
@@ -259,24 +314,24 @@
 							FILTER (?iri = <'.$iri.'>) .
 						}
 				';
-		
+
 				$rows = $GLOBALS['ONTOLOGIA_STORE']->query($query, 'rows');
-		
+
 				if ($errors = $GLOBALS['ONTOLOGIA_STORE']->getErrors())
 					throw new Exception($preMsg . "  Detalles:\n". join("\n", $errors));
-		
+
 				if (is_array($rows) && count($rows) > 0){
 					reset($rows);
 					return self::llenarConsumible(current($rows));
 				}
 				else
 					return null;
-		
+
 			} catch (Exception $e) {
 				error_log($e, 0);
 				return false;
 			}
 		}
-		
+
 	}
 ?>
