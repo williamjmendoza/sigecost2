f53f809776bb52f5fd3899dbdc50330f9a5f02e4
migdaliamedina <migdaeme@gmail.com>
2014-09-12 10:09:10 -0430

    Agregada funcion modificar en st de impresora en "desatascara papel"

--- vista/instancia/soporteTecnico/impresora/desatascarPapelInsertarModificar.php
+++ vista/instancia/soporteTecnico/impresora/desatascarPapelInsertarModificar.php
@@ -13,6 +13,14 @@
 	
 		<?php require ( SIGECOST_PATH_VISTA . '/general/head.php' ); ?>
 		
+		<script type="text/javascript">
+
+			function setAccion(accion) {
+				$('input[type="hidden"][name="accion"]').val(accion);
+			}
+
+    	</script>
+		
 	</head>
 	
 	<body>
@@ -21,8 +29,13 @@
 		
 		<div class="container">
 			<ul class="nav nav-tabs" role="tablist">
-				<li class="active"><a href="desatascarPapel.php?accion=insertar">Insertar</a></li>
+				<li 
+					<?php echo ($form->getTipoOperacion() == Formulario::TIPO_OPERACION_INSERTAR) ? ' class="active"' : ''; ?>
+					><a href="desatascarPapel.php?accion=insertar">Insertar</a></li>
 				<li><a href="desatascarPapel.php?accion=Buscar">Buscar</a></li>
+				<?php if($form->getTipoOperacion() == Formulario::TIPO_OPERACION_MODIFICAR) { ?>
+				<li class="active"><a href="#">Modificar</a></li>
+				<?php } ?>
 			</ul>
 		</div>
 		
@@ -36,7 +49,10 @@
 			
 			<form class="form-horizontal" role="form" method="post" action="desatascarPapel.php">
 				<div style="display:none;">
-					<input type="hidden" name="accion" value="guardar">
+					<input type="hidden" name="accion" value="">
+					<?php if($form->getTipoOperacion() == Formulario::TIPO_OPERACION_MODIFICAR) { ?>
+					<input type="hidden" name="iri" value="<?php echo $form->getSoporteTecnico()->getIri() ?>">
+					<?php } ?>
 				</div>
 				<div class="form-group">
 					<label class="control-label col-sm-3" for="iriEquipoReproduccion">En impresora:</label>
@@ -72,7 +88,11 @@
 				</div>
 				<div class="form-group">
 					<div class="col-sm-offset-3 col-sm-7">
-						<button type="submit" class="btn btn-primary">Guardar</button>
+						<?php if($form->getTipoOperacion() == Formulario::TIPO_OPERACION_INSERTAR) { ?>
+						<button type="submit" class="btn btn-primary" onclick="setAccion('guardar');">Guardar</button>
+						<?php } else if ($form->getTipoOperacion() == Formulario::TIPO_OPERACION_MODIFICAR) { ?>
+						<button type="submit" class="btn btn-primary" onclick="setAccion('actualizar');">Actualizar</button>
+						<?php } ?>
 					</div>
 				</div>
 			</form>
