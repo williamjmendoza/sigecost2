e1b80b6b0ac7b5b88ff882e67083511ad594e576
MigdaliaMedina <migdaeme@gmai.com>
2014-09-15 20:14:36 -0430

    "agregado cambio en mensaje de modificación exitosa"

--- modelo/instancia/elementoTecnologico/aplicacionGraficaDigitalDibujoDiseno.php
+++ modelo/instancia/elementoTecnologico/aplicacionGraficaDigitalDibujoDiseno.php
@@ -94,7 +94,7 @@
 					// Excepción porque no se pudieron guardar los datos actualizados de la instancia, para que se ejecute el Rollback
 					throw new Exception($preMsg . " No se pudieron guardar los datos actualizados de la instancia. Detalles:\n" . join("\n", $errors));
 
-				$GLOBALS['SigecostInfo']['general'][] = "Instancia de aplicación ofimática guardada satisfactoriamente.";
+				$GLOBALS['SigecostInfo']['general'][] = "Instancia de aplicación modificada satisfactoriamente.";
 				
 				// Commit de la transacción
 				return $instancia->getIri();
