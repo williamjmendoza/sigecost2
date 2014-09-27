66c8e7ae06877efbdb8684fdf00b84b47fbea013
MigdaliaMedina <migdaeme@gmai.com>
2014-09-27 17:37:18 -0430

    Funcion eliminar en st impresora "instalacion de impresora"

--- controlador/instancia/soporteTecnico/impresora/instalacionImpresora.php
+++ controlador/instancia/soporteTecnico/impresora/instalacionImpresora.php
@@ -54,7 +54,7 @@
 					if($resultado === false)
 						throw new Exception("La instancia no pudo ser actualizada");
 					
-					$GLOBALS['SigecostErrors']['general'] = "Instancia actualizada satisfactoriamente";
+					$GLOBALS['SigecostErrors']['general'][] = "Instancia actualizada satisfactoriamente.";
 					
 					$this->__desplegarDetalles($iri);
 					
@@ -112,6 +112,29 @@
 			}
 		}
 		
+		public function eliminar()
+		{
+			try
+			{
+				if( (!isset($_POST['iri'])) || (($iri=trim($_POST['iri'])) == '') )
+					throw new Exception("No se encontr&oacute; ning&uacute;n identificador para la instancia que desea eliminar.");
+				
+				// Eliminar la instancia de soporte técnico en impresora para instalación de impresora, de la base de datos
+				$resultado = ModeloInstanciaSTImpresoraInstalacionImpresora::eliminarInstancia($iri);
+					
+				if($resultado === false)
+					throw new Exception("La instancia no pudo ser eliminada.");
+				
+				$GLOBALS['SigecostInfo']['general'][] = "Instancia eliminada satisfactoriamente.";
+				
+				$this->buscar();
+				
+			} catch (Exception $e){
+				$GLOBALS['SigecostErrors']['general'][] = $e->getMessage();
+				$this->buscar();
+			}
+		}
+				
 		public function guardar()
 		{
 			$form = FormularioManejador::getFormulario(FORM_INSTANCIA_ST_IMPRESORA_INSTALACION_IMPRESORA_INSERTAR_MODIFICAR);
