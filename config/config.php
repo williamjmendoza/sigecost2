42b2b9fc9aa33c26b9491a078d8bcb114020d7c7
William - PC <william@gmail.com>
2015-03-23 15:03:20 -0430

    Cambio en el config. Se creo el archivo myconfig.php para separar las configuraciones personalizas, de las generales que están en config.php

--- config/config.php
+++ config/config.php
@@ -1,36 +1,36 @@
 <?php
 	
-	$GLOBALS['SIGECOST_CFG']["siteURL"] = 'http://localhost/sigecost2';
+	$GLOBALS['SIGECOST_CFG']["siteURL"] = '<URL completo del sigecost>';
 
 	// Configuración de la base de datos de patrones de soporte técnico
-	$GLOBALS['SIGECOST_CFG']["dbType"] = "mysql";
-	$GLOBALS['SIGECOST_CFG']["dbEncoding"] = "UTF8";
-	$GLOBALS['SIGECOST_CFG']["dbServer"] = "localhost";
-	$GLOBALS['SIGECOST_CFG']["dbPort"] = '3306';
-	$GLOBALS['SIGECOST_CFG']["dbUser"] = "root";
-	$GLOBALS['SIGECOST_CFG']["dbPass"] = "987654321";
-	$GLOBALS['SIGECOST_CFG']["dbDatabase"] = "patrones_st";
+	$GLOBALS['SIGECOST_CFG']["dbType"] = "<Tipo de manejador de base de datos>"; // Ejemplo: $GLOBALS['SIGECOST_CFG']["dbType"] = 'mysql'
+	$GLOBALS['SIGECOST_CFG']["dbEncoding"] = "<Codificación de caracteres de base de datos>"; // Ejemplo: $GLOBALS['SIGECOST_CFG']["dbEncoding"] = "UTF8";
+	$GLOBALS['SIGECOST_CFG']["dbServer"] = "<Servidor de base de datos>"; // Ejemplo: $GLOBALS['SIGECOST_CFG']["dbServer"] = "localhost";
+	$GLOBALS['SIGECOST_CFG']["dbPort"] = '<Puerto del servidor de base de datos>'; // Ejemplo: $GLOBALS['SIGECOST_CFG']["dbPort"] = '3306';
+	$GLOBALS['SIGECOST_CFG']["dbUser"] = "<Usuario de la base de datos>"; // Ejemplo: $GLOBALS['SIGECOST_CFG']["dbUser"] = "root";
+	$GLOBALS['SIGECOST_CFG']["dbPass"] = "<Password de la base de datos>"; // Ejemplo: $GLOBALS['SIGECOST_CFG']["dbPass"] = "123456789";
+	$GLOBALS['SIGECOST_CFG']["dbDatabase"] = "<Nombre de la base de datos>"; // $GLOBALS['SIGECOST_CFG']["dbDatabase"] = "patrones_st";
 	$GLOBALS['SIGECOST_CFG']["tablePrefix"] = "";
 	$GLOBALS['SIGECOST_CFG']["characterSet"] = "UTF-8";
 	$GLOBALS['SIGECOST_CFG']["timeZone"] = "+0:00";
 	$GLOBALS['SIGECOST_CFG']["displayTimeZone"] = "-04:30";
 	
 	// Configuración de la base de datos de arc2 (Ontología)
-	$GLOBALS['SIGECOST_CFG']["ontoDbServer"] = $GLOBALS['SIGECOST_CFG']["dbServer"];
-	$GLOBALS['SIGECOST_CFG']["ontoDbUser"] = $GLOBALS['SIGECOST_CFG']["dbUser"];
-	$GLOBALS['SIGECOST_CFG']["ontoDbPass"] = $GLOBALS['SIGECOST_CFG']["dbPass"];
-	$GLOBALS['SIGECOST_CFG']["ontoDbDatabase"] = "onto_st";
-	$GLOBALS['SIGECOST_CFG']["StoreName"] = "st";
+	$GLOBALS['SIGECOST_CFG']["ontoDbServer"] = '<Servidor de base de datos de la ontología>'; // Ejemplo: $GLOBALS['SIGECOST_CFG']["ontoDbServer"] = $GLOBALS['SIGECOST_CFG']["dbServer"];
+	$GLOBALS['SIGECOST_CFG']["ontoDbUser"] = '<Usuario de la base de datos de la ontología>'; // Ejemplo: $GLOBALS['SIGECOST_CFG']["ontoDbUser"] = $GLOBALS['SIGECOST_CFG']["dbUser"]
+	$GLOBALS['SIGECOST_CFG']["ontoDbPass"] = '<Password de la base de datos de la ontología>'; // Ejemplo: $GLOBALS['SIGECOST_CFG']["ontoDbPass"] = $GLOBALS['SIGECOST_CFG']["dbPass"];
+	$GLOBALS['SIGECOST_CFG']["ontoDbDatabase"] = '<Nombre de la base de datos de la ontología>'; // Ejemplo: $GLOBALS['SIGECOST_CFG']["ontoDbDatabase"] = "onto_st";
+	$GLOBALS['SIGECOST_CFG']["StoreName"] = '<Store name de la base de datos de la ontología (Prefijo)>'; // Ejemplo: $GLOBALS['SIGECOST_CFG']["StoreName"] = "st";
 	
 	// Array de configuración de la base de datos de arc2
 	$GLOBALS['SIGECOST_CFG']["ontoArc2Config"] = array(
 		// Configuración de la base de datos MySQL
-		'db_host' => $GLOBALS['SIGECOST_CFG']["ontoDbServer"],
-		'db_user' => $GLOBALS['SIGECOST_CFG']["ontoDbUser"],
-		'db_pwd' => $GLOBALS['SIGECOST_CFG']["dbPass"],
-		'db_name' => $GLOBALS['SIGECOST_CFG']["ontoDbDatabase"],
+		'db_host' => '<Servidor de base de datos de la ontología>', // Ejemplo: 'db_host' => $GLOBALS['SIGECOST_CFG']["ontoDbServer"],
+		'db_user' => '<Usuario de la base de datos de la ontología>', // Ejemplo: 'db_user' => $GLOBALS['SIGECOST_CFG']["ontoDbUser"], 
+		'db_pwd' => '<Password de la base de datos de la ontología>', // Ejemplo: 'db_pwd' => $GLOBALS['SIGECOST_CFG']["dbPass"], 
+		'db_name' => $GLOBALS['SIGECOST_CFG']["ontoDbDatabase"], // Ejemplo: 'db_name' => $GLOBALS['SIGECOST_CFG']["ontoDbDatabase"], 
 		// Configuración del arc2 store
-		'store_name' => $GLOBALS['SIGECOST_CFG']["StoreName"], 
+		'store_name' => '<Store name de la base de datos de la ontología (Prefijo)>',  // Ejemplo: 'store_name' => $GLOBALS['SIGECOST_CFG']["StoreName"],
 		// Configuración del SPARQL endpoint
 		'endpoint_features' => array(
 			'select', 'construct', 'ask', 'describe', // permite leer
@@ -53,4 +53,5 @@
 	$GLOBALS['SIGECOST_CFG']["truncamientoSolucionPatronSoporteTecnico"] = 30;
 	$GLOBALS['SIGECOST_CFG']["truncamientoSolucionPatronSTBusqueda"] = 300;
 	
+	require_once ("myconfig.php");
 ?>
